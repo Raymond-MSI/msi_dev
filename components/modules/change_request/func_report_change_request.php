94ed23f0e36e0071b2d6e4d267e4214ca8e38a7b
Raymond-MSI <raymond.raymond@techsupp.mastersystem.co.id>
2025-08-14 11:24:01 +0700

    fix_change_request

--- components/modules/change_request/func_report_change_request.php
+++ components/modules/change_request/func_report_change_request.php
@@ -1,38 +1,16 @@
 <?php
 
-function getReportChangeRequest($filters)
+function getReportChangeRequest()
 {
-
     $db_name = "change_request";
     $DBRCR = get_conn($db_name);
     if (!$DBRCR) {
-        die("Database connection not initialized.");
+        error_log("Failed to connect to the database: " . $db_name);
+        return []; // Return an empty array to prevent further errors
     }
-    $query = " SELECT DISTINCT
-        sa.gi_id,
-        sa.project_code,
-        sa.project_name,
-        sa.cr_no,
-        sa.type_of_service,
-        sa.so_number,
-        sa.order_number,
-        sa.type_of_service,
-        sa.requested_by_email,
-        scp.used_ticket,
-        scp.ticket_allocation,
-        scp.ticket_allocation_sisa,
-        sa.request_date,
-        sa.modified_date
-    FROM sa_general_informations sa
+    $query = "SELECT * FROM sa_general_informations sa
     LEFT JOIN sa_change_cost_plans scp
-        ON sa.cr_no = scp.cr_no 
-        ";
-    if (!empty($filters['change_request_approval_type2'])) {
-        $query .= " WHERE sa.change_request_approval_type2 = '" . $filters['change_request_approval_type2'] . "'";
-    }
+    ON sa.cr_no = scp.cr_no";
     $result = $DBRCR->get_sql($query);
-
-    
-
     return $result;
 }
